2dd5c8e5c0edbe9f568b8e749ea5b77594357c87
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2023-04-07 19:19:34 -0400

    Implement user logging in based on db

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -56,12 +56,19 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError("Could not identify user.", 401);
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    return next(new HttpError("Logging in failed."), 500);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    return next(new HttpError("Email or password not correct, please try again.", 401));
   }
 
   res.json({ message: "Logged in." });
