24002f671410d4d174afa1afe9dcbbe6cef84167
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2023-04-07 15:55:26 -0400

    Connect to MongoDB

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const HttpError = require('./models/http-error');
-
-const bodyParser = require('body-parser');
-
-const placesRoutes = require('./routes/places-routes');
-
+const HttpError = require("./models/http-error");
+const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
+const url = require("./constants/url");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes);
+app.use("/api/places", placesRoutes);
 
-app.use('/api/users', usersRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
   throw new HttpError("Cannot find this route.", 404);
 });
 
 app.use((error, req, res, next) => {
-    if (res.headerSent) {
-        return next(error);
-    }
-    res.status(error.code || 500);
-    res.json({message: error.message || 'An unkown error occurred!'});
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unkown error occurred!" });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    url
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
